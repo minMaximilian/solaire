84f9110d6a43f3962525e8fed49b51635de53da2
Tazer <yaseenelgamal20094@gmail.com>
2023-10-17 10:54:39 +0300

    Added a kind of remove template

--- kubejs/server_scripts/helpers.js
+++ kubejs/server_scripts/helpers.js
@@ -1,7 +1,6 @@
-const MOD = (domain, id, x) => (x ? `${x}x ` : "") + (id.startsWith('#') ? '#' : "") + domain + ":" + id.replace('#', '')
-
-const MC = (id, x) => MOD(`minecraft`, id, x)
-const C = (id, x) => MOD(`create`, id, x)
-const KJ = (id, x) => MOD(`kubejs`, id, x)
-const H = (id, x) => MOD(`tconstruct`, id, x)
-const S = (id, x) => MOD(`spelunkery`, id, x)
+const MC = id => `minecraft:${id}`
+const C = id => `create:${id}`
+const KJ = id => `kubejs:${id}`
+const FD = id => `farmersdelight:${id}`
+const S = id => `spelunkery:${id}`
+const ETC = id => `etcetera:${id}`
--- kubejs/startup_scripts/helpers.js
+++
@@ -1,5 +0,0 @@
-const MC = id => `minecraft:${id}`
-const C = id => `create:${id}`
-const KJ = id => `kubejs:${id}`
-const FD = id => `farmersdelight:${id}`
-const CS = id => `create_sa:${id}`
