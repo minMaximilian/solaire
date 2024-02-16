56f7f7da9d4dd1a6a174bc867794b2cd640adada
Tazer <yaseenelgamal20094@gmail.com>
2024-02-16 16:40:30 +0200

    kubejs changes

--- kubejs/server_scripts/helpers.js
+++ kubejs/server_scripts/helpers.js
@@ -1,6 +1,27 @@
-const MC = id => `minecraft:${id}`
-const C = id => `create:${id}`
-const KJ = id => `kubejs:${id}`
-const FD = id => `farmersdelight:${id}`
-const S = id => `spelunkery:${id}`
-const ETC = id => `etcetera:${id}`
+const MOD = (domain, id, x) => (x ? `${x}x ` : "") + (id.startsWith('#') ? '#' : "") + domain + ":" + id.replace('#', '')
+
+const MC = (id, x) => MOD(`minecraft`, id, x)
+const C = (id, x) => MOD(`create`, id, x)
+const KJ = (id, x) => MOD(`kubejs`, id, x)
+const H = (id, x) => MOD(`tconstruct`, id, x)
+const S = (id, x) => MOD(`spelunkery`, id, x)
+const ST = (id, x) => MOD(`sleep_tight`, id, x)
+
+const dyes = [
+    'white',
+    'light_gray',
+    'gray',
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'lime',
+    'green',
+    'cyan',
+    'light_blue',
+    'blue',
+    'purple',
+    'magenta',
+    'pink'
+]
--- kubejs/server_scripts/remove.js
+++ kubejs/server_scripts/remove.js
@@ -1,26 +1,20 @@
 console.info('Remove Script')
 
 ServerEvents.recipes(e => {
-	// // Remove recipes for all vanilla tools (stone and wooden tools don't have another recipe)
-	// const tools = ["shovel", "pickaxe", "axe", "hoe", "sword"];
-	// const materials = ["wooden", "stone", "iron", "golden", "diamond", "netherite"];
-	// materials.map(material => tools.map(tool => e.remove({output: MC(`${material}_${tool}`)})))
 
-	// // Remove recipes for raw ore into raw ore nuggets
-	// const rawOres = ["iron", "gold", "copper", "zinc", "magnetite"]
-	// rawOres.map(raw_ore => {
-	// 	e.remove({id: S(`raw_${raw_ore}`)}) 
-	// 	e.remove({id: S(`raw_${raw_ore}_nugget`)})
-	// })
-	// e.remove({id: S("raw_magnetite_from_nuggets")})
-})
+	// Remove recipes for all vanilla tools (stone and wooden tools don't have another recipe)
+	const tools = ["shovel", "pickaxe", "axe", "hoe", "sword"];
+	const materials = ["wooden", "stone", "iron", "golden", "diamond", "netherite"];
 
-function removeItemRecipes (item) {
-	ServerEvents.recipes(e => {
-	e.remove({input: item})
-	e.remove({output: item})
+	materials.map(material => tools.map(tool => e.remove({output: MC(`${material}_${tool}`)})))
+
+
+	// Remove recipes for making raw ores from their nuggets
+	const raw_ores = ["iron", "gold", "copper", "zinc", "magnetite"]
+
+	raw_ores.map(raw_ore => {
+		e.remove({id: S(`raw_${raw_ore}`)}) 
+		e.remove({id: S(`raw_${raw_ore}_nugget`)})
 	})
-}
-
-// Bismuth Stuff
-global.remove.map(item => removeItemRecipes(item))
+	e.remove({id: S("raw_magnetite_from_nuggets")}) // cause recipe ids dumb
+})
