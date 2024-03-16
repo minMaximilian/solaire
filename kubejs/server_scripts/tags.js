0db09bb97019fdd57ac4a4599b085f92086c03d7
Tazer <yaseenelgamal20094@gmail.com>
2024-03-16 21:28:54 +0200

    changes to mods

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Listen to item tag event
 ServerEvents.tags('item', tags => {
     tags.remove(SP('ropes'), SP('rope'))
@@ -10,16 +9,3 @@ ServerEvents.tags('item', tags => {
 			('flint', 'iron', 'gold', 'diamond', 'netherite')
 		].map(material => tags.removeAllTagsFrom(NTP(`${material}_knife`)))
 })
-=======
-// Listen to item tag event
-ServerEvents.tags('item', tags => {
-    event.remove(SP('ropes'), SP('rope'))
-
-	tags
-		.add(NTP('knives'), '#c:tools/knives')
-
-		[
-			('flint', 'iron', 'gold', 'diamond', 'netherite')
-		].map(material => tags.removeAllTagsFrom(NTP(`${material}_knife`)))
-})
->>>>>>> 16658b754d9728c4f14b23c45160d77d6a12374d
